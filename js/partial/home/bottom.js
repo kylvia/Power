a4319db404c419712cb2bd04505089e5ca1a0e8b
dengjie <984344996@qq.com>
2017-07-15 22:51:10 +0800

    1920分辨率调节

--- js/partial/home/bottom.js
+++ js/partial/home/bottom.js
@@ -4,5 +4,10 @@ define(function(){
 var bottom = {
     Render:function () {
 
+        console.log($('.content-2.cus-item').height());
+        $('.cus-bottom li > div').css('margin-top',($('.content-2.cus-item').height()-54)/2 + 'px')
+        $(window).resize(function() {
+            $('.cus-bottom li > div').css('margin-top',($('.content-2.cus-item').height()-54)/2 + 'px')
+        });
     }
 }
